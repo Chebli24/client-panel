77f24510660e10b0c63ff27287cb59c041a35a9e
Aya <user@Users-MBP.lan>
2020-08-25 17:43:41 +0300

    commit

---
+++ src/components/layout/AppNavbar.js
@@ -0,0 +1,14 @@
+import React, {Component } from 'react'
+
+class AppNavbar extends Component {
+    render(){
+        return (
+            <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
+                
+            </nav>
+            
+        )
+    }
+}
+
+export default AppNavbar
